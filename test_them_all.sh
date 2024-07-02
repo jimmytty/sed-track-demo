507ce9c19190ab37af648e9357b0a543f9106081
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2024-07-02 20:55:48 -0300

    * test_them_all.sh add check file exists

--- test_them_all.sh
+++ test_them_all.sh
@@ -3,6 +3,10 @@
 while read dir; do
     (
         cd $dir
+        if [[ ! -f test-$dir.bats ]]; then
+           echo "test-$dir.bats does not exists"
+           exit 1
+        fi
         bats test-$dir.bats | grep -m1 -q '^not ok' && tests='FAIL' || tests='OK'
         echo "$dir .... $tests"
     )
