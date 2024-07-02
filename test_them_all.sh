17a93cf07c77745af53e16c307e1d68dd36036d1
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2024-07-02 12:14:13 -0300

    * test_them_all.sh bugfixes

--- test_them_all.sh
+++ test_them_all.sh
@@ -3,9 +3,10 @@
 while read dir; do
     (
         cd $dir
-        bats test-$dir.bats &>/dev/null && tests='OK' || tests='FAIL'
-        echo "$dir ... $tests"
+        bats test-$dir.bats | grep -q '^not ok' && tests='FAIL' || tests='OK'
+        echo "$dir .... $tests"
     )
 done < <(
-    find . -mindepth 1 -maxdepth 1 -type d -not -name '.git' -printf '%f\n'
+    find . -mindepth 1 -maxdepth 1 -type d -not -name '.git' -printf '%f\n' |
+        sort
 )
